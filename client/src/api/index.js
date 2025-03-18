3b85be6014b6505623eef4b504fe5a0995b541f9
saideep278 <saideepchakilam278@gmail.com>
2025-03-18 11:13:46 +0530

    d

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080/api/v1/' });
+const API = axios.create({ baseURL: 'http://13.232.91.144:8080/api/v1/' });
 
 
 export const fetchPosts = () => API.get(`/posts`);
